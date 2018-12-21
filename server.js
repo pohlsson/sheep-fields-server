267da656bbc4ec7572d0558dc2812fdfa610cb4d
patrik <patrik.olsson@axiomatics.com>
2018-12-21 14:31:12 +0100

    Mock get sheep

--- server.js
+++ server.js
@@ -5,26 +5,20 @@ const bodyParser = require("body-parser");
 const API_PORT = 3001;
 const app = express();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'sheep_field'
-});
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/sheep', (req, res) => {
-  connection.connect();
-  connection.query('select * from sheep', (error, results, fields) => {
-    if (error) {
-      throw error;
-    }
-    res.send(JSON.stringify(results));
-  });
-  connection.end();
+  const sheep = {
+    id: '0',
+    name: 'Poonard',
+    x_position: '200',
+    y_position: '100',
+  };
+  const response = {
+    sheep: [sheep],
+  };
+  res.send(response);
 });
-
 
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
